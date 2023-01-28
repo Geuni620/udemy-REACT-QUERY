62a6dcaca9cbdc3454045cb7a4df1d93c609eaa1
Geuni <dlrmsgnl620@naver.com>
2023-01-28 17:23:40 +0900

    18. mutation 포스팅 제목 업데이트 하기

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -32,6 +32,7 @@ export function PostDetail({post}) {
   );
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   if (isLoading) return <div>...Loading</div>;
   if (isError)
@@ -41,8 +42,6 @@ export function PostDetail({post}) {
         <p>{error.toString()}</p>
       </>
     );
-
-  console.log(data);
 
   return (
     <>
@@ -58,7 +57,18 @@ export function PostDetail({post}) {
         <p style={{color: "green"}}>Post has (not) been deleted</p>
       )}
 
-      <button>Update title</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{color: "red"}}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{color: "purple"}}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{color: "green"}}>Post has (not) been updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
