994d30a54a4886642f74aac35d144e6b169f63b3
Geuni <dlrmsgnl620@naver.com>
2023-01-29 16:27:46 +0900

    32. React Query 설치 및 설정

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+import { queryClient } from '../../react-query/queryClient';
 import { theme } from '../../theme';
 import { Loading } from './Loading';
 import { Navbar } from './Navbar';
@@ -9,9 +12,29 @@ import { Routes } from './Routes';
 export function App(): ReactElement {
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
+
+/*
+  
+  react-query dev tools
+    - NODE_ENV가 Production으로 설정되었을 때는 표시되지 않음.
+    - CRA에서 npm start를 실행하면, NODE_ENV는 개발모드로 실행함
+
+*/
+
+/*
+
+  custom hooks를 사용하는 이유
+    - 다수의 useQuery 호출을 사용했다면, 사용 중인 키의 종류가 헷갈릴 수 있음
+    - custom hooks을 사용해 매번 호출한다면 키를 헷갈릴 위험이 없음.
+    - 사용하길 원하는 쿼리 함수를 혼동하는 위험도 없음.
+
+*/
