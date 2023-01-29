601f94f0985604be6c79d5085954ab286a2c89aa
Geuni <dlrmsgnl620@naver.com>
2023-01-29 16:38:39 +0900

    35. useIsFetching을 사용하는 중앙 집중식 페칭 표시기(Indicator)

--- base-lazy-days/client/src/components/app/Loading.tsx
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,9 +1,9 @@
 import { Spinner, Text } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { useIsFetching } from 'react-query';
 
 export function Loading(): ReactElement {
-  // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
+  const isFetching = useIsFetching();
 
   const display = isFetching ? 'inherit' : 'none';
 
@@ -25,3 +25,17 @@ export function Loading(): ReactElement {
     </Spinner>
   );
 }
+
+/*
+  
+  useIsFetching은 현재 가져오기 상태인 쿼리 호출의 수를 나타내는 정수값을 반환함.
+  - isFetching이 0보다 크다면, 가져오기 상태인 호출이 존재하며, 참으로 평가될 것
+    const display = isFetching ? 'inherit' : 'none';
+    - 이 경우 display는 inherit으로 설정되어 로딩 스피너를 표시하게 됨
+  - 현재 가져오는 항목이 없다면, isFetching은 거짓으로 평가되는데 0이 거짓이기 때문.
+
+  - 윈도우(dev tools)를 클릭만 했는데 왜 로딩이 진행될까?
+    - 이건 React-query와 기본으로 함께 따라오는 리페칭 구성으로, 윈도우에 다시 돌아올 때 데이터를 다시 가져오게 됨.
+  - 
+
+*/
