67a9178dbcd7968a7d6f8b263aecfc250d2593a1
Geuni <dlrmsgnl620@naver.com>
2023-01-29 16:28:22 +0900

    33. 커스텀 쿼리 훅:useTreatments / 34. 폴백 데이터

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,28 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
-  return [];
+
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+
+  return data;
 }
+
+/*
+
+  useQuery를 사용할 때 인수를 전달하지 않으므로, 인수를 전달하기 위해 익명 함수를 호출할 필요는 없음
+  () => getTreatments() → getTreatments
+
+*/
