50697774426c4278a9cc9559b9d86ec2779dfd46
이근휘 <dlrmsgnl620@naver.com>
2023-01-31 12:17:57 +0900

    66. 추가 'mutate' 함수를 위한 TypeScript

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { UseMutateFunction, useMutation } from 'react-query';
 
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -19,14 +19,16 @@ async function setAppointmentUser(
   });
 }
 
-// TODO: update type for React Query mutate function
-type AppointmentMutationFunction = (appointment: Appointment) => void;
-
-export function useReserveAppointment(): AppointmentMutationFunction {
+export function useReserveAppointment(): UseMutateFunction<
+  void,
+  unknown,
+  Appointment,
+  unknown
+> {
   const { user } = useUser();
   const toast = useCustomToast();
 
-  const { mutate } = useMutation((appointment) =>
+  const { mutate } = useMutation((appointment: Appointment) =>
     setAppointmentUser(appointment, user?.id),
   );
   /*
@@ -42,6 +44,35 @@ export function useReserveAppointment(): AppointmentMutationFunction {
   */
 
   return mutate;
+
+  /*
+    66. mutate 함수를 위한 typeScript
+      - custom hook에서 mutate 함수를 반환하는 유형은 다음과 같음.
+        - useMutateFunction에는 몇 가지 매개변수가 있는데, 
+          1. 변이 함수 자체에서 반환된 데이터(Date) 유형: 이 경우 변이 함수는 데이터를 반환하지 않음, 따라서 void로 설정.
+          2. 발생할 것으로 예상되는 오류 유형 : 이것은 Error로 설정.
+          3. 변수(Variables) : 이것은 mutate 함수가 예상하는 변수 유형임.
+            - 위 경우 mutate 함수에 Appointment를 전달하죠. 
+          4. 컨텍스트(Context) : 이것은 낙관적 업데이트 롤백을 위해, onMutate에 설정하는 유형임.
+            - 추후 Optimistic(낙관적) 업데이트 강의에서 또 살펴봄
+            - 컨텍스트에 대한 Appointment 유형을 저장할 것이기 때문에 지금 추가했음.
+
+      - 이제 위 내용에서 본 유형을 반환해보자
+      export function useReserveAppointment(): UseMutateFunction<> {
+        const { user } = useUser();
+        const toast = useCustomToast();
+
+        const { mutate } = useMutation((appointment) =>
+          setAppointmentUser(appointment, user?.id),
+        );
+      - useMutateFunction<>를 입력해야함, 이 type은 react-query에서 가져와야함.
+        - 1. 위에서 봤듯이 변이 함수에서 반환된 데이터의 경우 void임.
+        - 2. 오류 유형은 unknown 상태로 두겠음. 이전에 봤듯이 오류 유형과 onError 핸들러를 확인했었음.
+        - 3. 다음은 mutate 함수에서 이 mutation 함수로 전달될 변수임, 따라서 Appointment 유형이 됨.
+        - 4. 그런 다음 마지막으로 컨텍스트 유형이 있음, 이 유형은 unknown으로 남겨두었다가 낙관적 업데이트를 할 때 내용을 채워 넣겠음.
+      
+      - 이렇게 했을 때 여전히, 에러가 존재하는데 appointment 매개변수에 빨간 줄이 뜸 → 인수 Type을 구체적으로 입력해 주면 해결가능.
+  */
 }
 
 /*
