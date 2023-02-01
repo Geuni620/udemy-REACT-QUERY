f1c216fda791732a5c864f9382a3c6292f4ed1e1
이근휘 <dlrmsgnl620@naver.com>
2023-02-01 11:29:53 +0900

    69. 코드 퀴즈! 변이(mutation)로 예약 취소하기

--- base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -1,22 +1,47 @@
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: 'remove', path: '/userId' }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
-// TODO: update return type
-export function useCancelAppointment(): (appointment: Appointment) => void {
+export function useCancelAppointment(): UseMutateFunction<
+  void,
+  unknown,
+  Appointment,
+  unknown
+> {
+  const queryClient = useQueryClient();
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation(
+    (appointments: Appointment) => removeAppointmentUser(appointments),
+    /*
+      사실 이 부분은 이렇게도 수정할 수 있음
+      const {mutate} = useMutation(removeAppointmentUser)
+
+
+      그리고 UseMutateFunction<void, unknown, Appointment, unknown> 중 마지막 unknown은 context부분임, 여기가 unknown인 이유는 onMutate 함수를 실행하지 않았기 때문.
+      - 이 부분은 낙관적 업데이트에 대해 이야기 할 때 더 자세히 다룰 예정.
+    */
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([queryKeys.appointments]);
+        toast({
+          title: 'You have canceled the appointment!',
+          status: 'warning',
+        });
+      },
+    },
+  );
+
+  return mutate;
 }
